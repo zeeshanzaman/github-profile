201a64377ce88776b7b3dc19059fa60c2400dcdf
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-26 19:52:45 +0500

    create card HTML

--- custom.js
+++ custom.js
@@ -1,6 +1,7 @@
 // root url
 const APIURL = 'https://api.github.com/users/'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -8,11 +9,39 @@ async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username)
 
-        console.log(data)
+        createUserCard(data)
     } catch (err) {
         console.log(err)
     }
 }
+
+function createUserCard(user) {
+    const cardHTML = `<div class="card">
+    <div>
+        <img src="https://randomuser.me/api/protraits/men/30.jpg" alt="" class="avatar">
+    </div>
+    <div class="user-info">
+        <h2>johndoe</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quas.</p>
+
+        <ul>
+            <li>300 <strong>Followers</strong> </li>
+            <li>100 <strong>Following</strong> </li>
+            <li>30 <strong>Repos</strong> </li>
+        </ul>
+
+        <div id="repos">
+            <a href="#" class="repose">Repo 1</a>
+            <a href="#" class="repose">Repo 2</a>
+            <a href="#" class="repose">Repo 3</a>
+        </div>
+    </div>
+</div>`
+
+    main.innerHTML = cardHTML
+
+}
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
